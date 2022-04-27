46c9f2c425689a0324d0fc6d926bec3f4035835c
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2022-04-27 13:39:12 +0900

    애니메이션 성공
    다른 애니메이션을 사용하기 위해서
    fbx를 미리 로드 시켜 놓고 메쉬 데이터를 넘겨 벡터에 저장 후 그것을 사용하도록 만듬.

--- 01. Project/Engine/PlayerScript.cpp
+++ 01. Project/Engine/PlayerScript.cpp
@@ -3,6 +3,8 @@
 
 #include "BulletScript.h"
 #include "TestScript.h"
+#include "RenderMgr.h"
+#include "Animator3D.h"
 
 CPlayerScript::CPlayerScript()
 	: CScript((UINT)SCRIPT_TYPE::PLAYERSCRIPT)
@@ -32,32 +34,63 @@ void CPlayerScript::update()
 
 	Vec2 vDrag = CKeyMgr::GetInst()->GetDragDir();
 	Vec3 vRot = Transform()->GetLocalRot();
-
+	
 	//Vec3 vPos = Transform()->GetLocalPos();
 	//Vec3 vRot = Transform()->GetLocalRot();
-
-	if (KEY_HOLD(KEY_TYPE::KEY_UP))
+	if (KEY_HOLD(KEY_TYPE::KEY_W))
 	{
 		WorldDir = -playerTrans->GetWorldDir(DIR_TYPE::FRONT);
 		localPos += WorldDir * 200.f * DT;
+
+		if (KEY_HOLD(KEY_TYPE::KEY_LSHIFT))
+		{
+			localPos += WorldDir * 200.f * DT;
+			SetPlayerAnimation(2);
+		}
+		else SetPlayerAnimation(1);
 	}
 
-	if (KEY_HOLD(KEY_TYPE::KEY_DOWN))
+	else if (KEY_HOLD(KEY_TYPE::KEY_S))
 	{
 		WorldDir = playerTrans->GetWorldDir(DIR_TYPE::FRONT);
 		localPos += WorldDir * 200.f * DT;
+
+		if (KEY_HOLD(KEY_TYPE::KEY_LSHIFT))
+		{
+			localPos += WorldDir * 200.f * DT;
+			SetPlayerAnimation(2);
+		}
+		else SetPlayerAnimation(1);
 	}
 
-	if (KEY_HOLD(KEY_TYPE::KEY_LEFT))
+	else if (KEY_HOLD(KEY_TYPE::KEY_A))
 	{	
 		WorldDir = playerTrans->GetWorldDir(DIR_TYPE::RIGHT);
 		localPos += WorldDir * 200.f * DT;
+
+		if (KEY_HOLD(KEY_TYPE::KEY_LSHIFT))
+		{
+			localPos += WorldDir * 200.f * DT;
+			SetPlayerAnimation(2);
+		}
+		else SetPlayerAnimation(1);
 	}
 
-	if (KEY_HOLD(KEY_TYPE::KEY_RIGHT))
+	else if (KEY_HOLD(KEY_TYPE::KEY_D))
 	{
 		WorldDir = -playerTrans->GetWorldDir(DIR_TYPE::RIGHT);
 		localPos += WorldDir * 200.f * DT;
+
+		if (KEY_HOLD(KEY_TYPE::KEY_LSHIFT))
+		{
+			localPos += WorldDir * 200.f * DT;
+			SetPlayerAnimation(2);
+		}
+		else SetPlayerAnimation(1);
+	}
+	else
+	{
+		SetPlayerAnimation(0);
 	}
 
 	if (KEY_HOLD(KEY_TYPE::KEY_LBTN))
@@ -66,7 +99,22 @@ void CPlayerScript::update()
 		Transform()->SetLocalRot(vRot);
 	}
 
+	if (KEY_HOLD(KEY_TYPE::KEY_SPACE))
+	{
+		SetPlayerAnimation(3);
+	}
+
 	Transform()->SetLocalPos(localPos);
 
+
+
 	//Transform()->SetLocalRot(vRot);
+}
+
+void CPlayerScript::SetPlayerAnimation(const int i)
+{
+	if (m_pAniData.size() == 0)	return;
+	Animator3D()->SetBones(m_pAniData[i]->GetBones());
+	Animator3D()->SetAnimClip(m_pAniData[i]->GetAnimClip());
+	MeshRender()->SetMesh(m_pAniData[i]);
 }
--- 01. Project/Engine/PlayerScript.h
+++ 01. Project/Engine/PlayerScript.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "Script.h"
+#include "Mesh.h"
 
 class CPlayerScript :
 	public CScript
 {
+public:
+	vector<Ptr<CMesh>>	m_pAniData;
+
 private:
 	Ptr<CMaterial>		m_pOriginMtrl;
 	Ptr<CMaterial>		m_pCloneMtrl;
@@ -11,6 +15,9 @@ private:
 public:
 	virtual void awake();	
 	virtual void update();
+
+	void GetPlayerAnimation(Ptr<CMesh> AniDate) { m_pAniData.push_back(AniDate); };
+	void SetPlayerAnimation(const int i);
 
 public:
 	CLONE(CPlayerScript);
